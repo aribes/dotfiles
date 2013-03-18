d43a48fce435478071d709d7bc7f43ebd4e389f6
Andre Ribes <andre.ribes@tibra.com>
2013-03-18 14:42:11 +1100

    Start adding awesome 3.5

---
+++ awesome/ubuntu-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo Installing awesome packages for ubuntu
+
+sudo aptitude install lua5.2 lua-lgi
+sudo aptitude install libgirepository1.0-dev
+sudo aptitude install libx11-dev libxinerama-dev libxrandr-dev libpango1.0-dev libimlib2-dev libgtk2.0-dev libxcb-shm0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-shape0-dev libcairo2-dev libxcb-xinerama0-dev liblua5.1-filesystem0 liblua5.1-logging libdirectfb-dev libxt-dev libx11-xcb-dev
+sudo aptitude install libxcb-util0-dev libxcb-keysyms1-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-xtest0-dev libxdg-basedir-dev
+sudo aptitude install libstartup-notification0-dev
+sudo aptitude install asciidoc
---
+++ install-awesome.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo Installing packages
+
+echo Compiling Awesome
+
+echo Creating .xinitrc/.xsession
+
+ln -s ~/.xinitrc ~/.xsession
+
+echo Installing Awesome configuration file
