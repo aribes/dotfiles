d03805b6e657755137955801896dc015f84a7938
Andre Ribes <andre.ribes@tibra.com>
2013-04-26 10:48:02 +1000

    Start awesome

--- install-awesome.sh
+++ install-awesome.sh
@@ -1,11 +1,52 @@
 #!/bin/sh
 
-echo Installing packages
+echo "Installing packages"
 
-echo Compiling Awesome
+echo "Compiling Awesome"
 
-echo Creating .xinitrc/.xsession
+echo "Creating .xinitrc/.xsession"
 
-ln -s ~/.xinitrc ~/.xsession
+if [ -e $HOME/.xinitrc ];  then
+    if [ -f $HOME/.xinitrc ];  then
+        echo "Saving old .xinitrc file"
+        mv $HOME/.xinitrc $HOME/.xinitrc-save
+    fi
+fi
 
-echo Installing Awesome configuration file
+if [ -h $HOME/.xinitrc ];  then
+    echo "Removing .xinitrc link"
+    rm $HOME/.xinitrc
+fi
+
+if [ -e $HOME/.xsession ];  then
+    if [ -f $HOME/.xsession ];  then
+        echo "Saving old .xsession file"
+        mv $HOME/.xsession $HOME/.xsession-save
+    fi
+fi
+
+if [ -h $HOME/.xsession ];  then
+    echo "Removing .xsession link"
+    rm $HOME/.xsession
+fi
+
+touch $HOME/.xinitrc
+echo "#!/bin/sh" >> $HOME/.xinitrc
+echo exec /usr/local/bin/awesome >> $HOME/.xinitrc
+ln -s $HOME/.xinitrc $HOME/.xsession
+
+echo "Installing Awesome configuration file"
+
+if [ -e $HOME/.config/awesome/rc.lua ];  then
+    if [ -f $HOME/.config/awesome/rc.lua ];  then
+        echo "Saving old rc.lua file"
+        mv $HOME/.config/awesome/rc.lua $HOME/.config/awesome/rc.lua-save
+    fi
+fi
+
+if [ -h $HOME/.config/awesome/rc.lua ];  then
+    echo "Removing rc.lua link"
+    rm $HOME/.config/awesome/rc.lua
+fi
+
+ln -s $HOME/.dotfilesdir/awesome/rc.lua $HOME/.config/awesome/rc.lua
